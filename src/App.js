138de5431121c1b74c5c2164e4cff36f5e44b505
Samuel Ostholm <sostholm@gmail.com>
2020-02-18 21:04:22 +0100

    working search component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { initializeIcons } from '@uifabric/icons';
 
+import { SearchBox } from 'office-ui-fabric-react/lib/SearchBox';
 import { ApolloProvider } from '@apollo/react-hooks'
-import { useQuery } from '@apollo/react-hooks';
+import { useQuery, useLazyQuery } from '@apollo/react-hooks';
+// import {ALL_MICROBES, MICROBES_FILTERED_BY_SPECIES} from './queries'
 import { gql } from 'apollo-boost'
 
+initializeIcons();
 
-const ALL_MICROBES = gql`
-  {
-    allMicrobes{
+// ALL_MICROBES
+const MICROBES_FILTERED_BY_SPECIES = gql`
+query Microbe($species: String!){
+    allMicrobes(species: $species){
       edges{
         node{
           fullName
@@ -18,21 +23,53 @@ const ALL_MICROBES = gql`
     }
   }
 `
-
 function AllMicrobes(){
-  const {loading, error, data} = useQuery(ALL_MICROBES)
+  const [search, setSearch] = useState('')
+  const [results, setResults] = useState(null)
+  const [getMicrobes, {loading, error, data}] = useLazyQuery(MICROBES_FILTERED_BY_SPECIES)
 
+  // { variables: { breed: 'bulldog' } }
+  useEffect(()=>{
+    if (data && data.allMicrobes) {
+      console.log('data', data)
+      setResults(data);
+    }
+  },[data])
+  
   if(loading) return <p>Loading...</p>
   if(error) return <p>Error:(</p>
-
-  return data.allMicrobes.edges.map(node => (
-    <div key={node.node.fullName}>
-      <p>{node.node.fullName}</p>
+  
+    
+  const Results = (props) => {
+    return props.data? props.data.allMicrobes.edges.map(node => (
+      <div key={node.node.fullName}>
+        <p>{node.node.fullName}</p>
+      </div> 
+    )) : null
+  }
+  return (
+    <div>
+      <SearchBox
+        styles={{ root: { width: 200 } }}
+        placeholder="Search"
+        onEscape={ev => {
+          console.log('Custom onEscape Called');
+        }}
+        onClear={ev => {
+          console.log('Custom onClear Called');
+        }}
+        onChange={(_, newValue) => setSearch(newValue)}
+        onSearch={newValue => getMicrobes({ variables: { species: search }})}
+        onFocus={() => console.log('onFocus called')}
+        onBlur={() => console.log('onBlur called')}
+      />
+      <Results data={results}/>
     </div>
-  ))
+  )
 }
 
 function App() {
+
   return (
     <div className="App">
       <AllMicrobes />
